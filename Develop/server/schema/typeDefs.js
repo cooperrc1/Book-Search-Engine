45477229ed409c435363033ea0fca659b8d4ba97
cooperrc <coopercutbirth@yahoo.com>
2021-08-03 22:50:23 -0500

    first commit, adding apollo server

---
+++ Develop/server/schema/typeDefs.js
@@ -0,0 +1,14 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+type User {
+    _ID: ID
+    username: String
+    email: String
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
